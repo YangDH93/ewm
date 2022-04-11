58f141bf7e59c77da1d47755a51950c220b2b5a0
YD <fkrtnl123@naver.com>
2022-04-11 20:47:19 +0900

    양 수정

--- ewm/EWMMain.java
+++ ewm/EWMMain.java
@@ -12,8 +12,8 @@ public class EWMMain {
 		Menu m = null;
 
 		while(true) {
-			System.out.print("1.오늘 뭐 먹지?(say 양동훈)\n2.오늘 뭐 먹지?(say 방지훈)"
-					+ "\n3.오늘 뭐 먹지?(say 최지원)\n4.오늘 뭐 먹지?(say 예린홍)\n5.뽑기 시작\n>>> ");
+			System.out.print("오늘 뭐 먹지?\n1.say 양동훈\n2.say 방지훈"
+					+ "\n3.say 최지원\n4.say 예린홍\n5.뽑기 시작\n>>> ");
 			choice = sc.nextInt();
 			switch (choice) {
 			case 1: { // 오늘 뭐 먹 양동훈
