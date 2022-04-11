853ebce34e629444adb951adfa2a68e6957754f2
YD <fkrtnl123@naver.com>
2022-04-11 20:30:18 +0900

    동훈 양 완

--- ewm/EWMMain.java
+++ ewm/EWMMain.java
@@ -2,28 +2,40 @@ package ewm;
 
 import java.util.Scanner;
 
-public class EWMMain {
-   public static void main(String[] args) {
+import yangMenu.Yang;
 
-      Scanner sc = new Scanner(System.in);
-      int choice = 0;
-      while(true) {
-         switch (choice) {
-         case 1: { // 오늘 뭐 먹 양동훈
-            
-         }break; 
-         case 2: { // 내일 뭐 먹 최지훈
-            
-         }break;
-         case 3: { // 모레 뭐 먹 방지원
-            
-         }break;
-         case 4: { // 싫어하는 명단 예린홍
-            
-         }break;
-         
-         }
-         
-      }
-   }
+public class EWMMain {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int choice = 0;
+		Menu m = null;
+
+		while(true) {
+			System.out.print("1.오늘 뭐 먹지?(say 양동훈)\n2.오늘 뭐 먹지?(say 방지훈)"
+					+ "\n3.오늘 뭐 먹지?(say 최지원)\n4.오늘 뭐 먹지?(say 예린홍)\n5.뽑기 시작\n>>> ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1: { // 오늘 뭐 먹 양동훈
+				m = new Yang();
+				m.menu();
+			}break; 
+			case 2: { // 오늘 뭐 먹 방지훈
+
+			}break;
+			case 3: { // 오늘 뭐 먹 최지원
+
+			}break;
+			case 4: { // 오늘 뭐 먹 예린홍
+
+			}break;
+
+			}
+			if(choice == 5) break;
+
+		}// while문 종료 
+		int num = (int) (Math.random()*4+1);
+		System.out.println(num + "번 당첨~!");
+		sc.close();
+	}
 }
---
+++ yangMenu/Yang.java
@@ -0,0 +1,11 @@
+package yangMenu;
+
+import ewm.Menu;
+
+public class Yang implements Menu{
+	
+	@Override
+	public void menu() {
+		System.out.println("오늘 뭐 먹지 ~ 흠.. \n애들이 먹자는거 먹어여겠다!");
+	}
+}
